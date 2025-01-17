009d5013a245442f1bf1f604fe3fb07a2aa1d0fd
JorMaister <jorgejjs86@gmail.com>
2025-01-17 17:00:02 +0100

    working

--- gapstop/protocols/protocol_gapstop_tm.py
+++ gapstop/protocols/protocol_gapstop_tm.py
@@ -28,21 +28,21 @@ import logging
 from enum import Enum
 from os.path import abspath, join, basename
 from typing import Union, List
-
 import numpy as np
-from SCons.Tool import suffix
 from emtable import Table
-
 from gapstop import Plugin
+from pwem.convert import transformations
 from pwem.emlib.image import ImageHandler
 from pwem.objects import VolumeMask, Volume
 from pwem.protocols import EMProtocol
 from pyworkflow import BETA
-from pyworkflow.object import Pointer
+from pyworkflow.object import Pointer, Set
 from pyworkflow.protocol import STEPS_PARALLEL, PointerParam, FloatParam, StringParam, IntParam, GPU_LIST
 from pyworkflow.utils import Message, makePath, getExt, createLink, cyanStr, removeBaseExt
 from scipion.constants import PYTHON
-from tomo.objects import SetOfCoordinates3D, SetOfTomograms, SetOfTiltSeries, CTFTomo, SetOfCTFTomoSeries
+from tomo.constants import BOTTOM_LEFT_CORNER
+from tomo.objects import SetOfCoordinates3D, SetOfTomograms, SetOfTiltSeries, CTFTomo, SetOfCTFTomoSeries, Coordinate3D, \
+    Tomogram
 from tomo.utils import getObjFromRelation
 
 logger = logging.getLogger(__name__)
@@ -56,6 +56,7 @@ IN_MASK = 'mask'
 MRC = '.mrc'
 EM = '.em'
 TLT = '.tlt'
+NPY = '.npy'
 # tm_param.star columns
 ROOTDIR = "rootdir"
 OUTPUTDIR = "outputdir"
@@ -79,6 +80,7 @@ RESULTS_DIR = 'results'
 SCORES = 'scores'
 ANGLES = 'angles'
 PARTICLE_LIST_FILE = 'particleList'
+PARTICLES_DATA_FILE = 'particlesData'
 
 class gapStopOutputs(Enum):
     coordinates = SetOfCoordinates3D
@@ -127,7 +129,7 @@ class ProtGapStopTemplateMatching(EMProtocol):
                       help='Used to get the tilt angles. If empty, the protocol will try to reach, via relations, '
                            'the tilt-series associated to the introduced tomograms.')
         form.addParam(REF_VOL, PointerParam,
-                      pointerClass='Volume',
+                      pointerClass='Volume, SubTomogram',
                       important=True,
                       label="Reference volume")
         form.addParam(IN_MASK, PointerParam,
@@ -186,6 +188,7 @@ class ProtGapStopTemplateMatching(EMProtocol):
     # --------------------------- INSERT steps functions ----------------------
     def _insertAllSteps(self):
         self._initialize()
+        closeSetStepDeps = []
         cRId = self._insertFunctionStep(self.convertReferenceStep,
                                         prerequisites=[],
                                         needsGPU=False)
@@ -202,6 +205,13 @@ class ProtGapStopTemplateMatching(EMProtocol):
             peId = self._insertFunctionStep(self.extractCoordinatesStep, tsId,
                                             prerequisites=tmId,
                                             needsGPU=False)
+            cOutId = self._insertFunctionStep(self.createOutputStep, tsId,
+                                              prerequisites=peId,
+                                              needsGPU=False)
+            closeSetStepDeps.append(cOutId)
+        self._insertFunctionStep(self._closeOutputSet,
+                                 prerequisites=closeSetStepDeps,
+                                 needsGPU=False)
 
     # -------------------------- STEPS functions ------------------------------
     def _initialize(self):
@@ -343,6 +353,7 @@ drop_nan_columns=True)
         scoresMapFile = self._getResultsFile(tsId, f'{SCORES}_0_{tomoObjId}')
         anglesMapFile = self._getResultsFile(tsId, f'{ANGLES}_0_{tomoObjId}')
         outParticleListFile = self._getResultsFile(tsId, PARTICLE_LIST_FILE)
+        # TODO: Beware the scores threshold
         codePatch = f"""
 from cryocat import tmana        
 
@@ -353,7 +364,7 @@ angles_list='{self._getCryoCatAngleFile()}',
 tomo_id={tomo.getObjId()},
 particle_diameter={self.partDiameter.get()},
 object_id=None,
-scores_threshold=None,
+scores_threshold=0.12,
 sigma_threshold=None,
 cluster_size=None,
 n_particles=None,
@@ -368,8 +379,94 @@ angles_numbering=0)
             pyFile.write(codePatch)
         Plugin.runGapStop(self, PYTHON, extractCoordsPythonFile, isCryoCatExec=True)
 
+    def createOutputStep(self, tsId: str):
+        with self._lock:
+            tomo = self.tomoDict[tsId]
+            outCoords = self.createOutputSet()
+            # The particle list is encoded in a .em file. Thus, it will be loaded and decoded.
+            particlesDataNpyFile = self._loadParticleList(tsId)
+            particlesData = np.load(particlesDataNpyFile)[0, :, :]  # Dims are [1, N, M] -> remove the first dim -> 2D
+            for row in particlesData:
+                coord = self._createCoordFromRow(tomo, row)
+                outCoords.append(coord)
+            outCoords.write()
+            self._store(outCoords)
 
     # --------------------------- UTILS functions -----------------------------
+    def createOutputSet(self):
+        outCoords = getattr(self, self._possibleOutputs.coordinates.name, None)
+        if outCoords:
+            outCoords.enableAppend()
+        else:
+            inTomosPointer = self._getFormAttrib(IN_TOMOS, returnPointer=True)
+            inTomos = inTomosPointer.get()
+            outCoords = SetOfCoordinates3D.create(self._getPath(), template="coordinates%s")
+            outCoords.setPrecedents(inTomos)
+            outCoords.setSamplingRate(inTomos.getSamplingRate())
+            outCoords.setBoxSize(self._getFormAttrib(REF_VOL).getDim()[0])
+            outCoords.setStreamState(Set.STREAM_OPEN)
+
+            self._defineOutputs(**{self._possibleOutputs.coordinates.name: outCoords})
+            self._defineSourceRelation(inTomosPointer, outCoords)
+
+        return outCoords
+
+    def _createCoordFromRow(self, tomo: Tomogram, row: np.array):
+        """The particle list is encoded in a .em file. Thus, it will be loaded and decoded. The data represented
+        on each column is explained in https://cryocat.readthedocs.io/latest/user_guide/motl_basics.html:
+        Columns:
+        0 - "score” - a quality metric (typically cross-correlation value between the particle and the reference)
+        1 - "geom1” - a free geometric property
+        2 - "geom2” - a free geometric property
+        3 - "subtomo_id” - a subtomogram id; IMPORTANT many functions rely on this one to be unique
+        4 - "tomo_id” - a tomogram id to which the particle is affiliated to
+        5 - "object_id” - an object id to which the particle is affiliated to
+        6 - "subtomo_mean” - a mean value of the subtomogram
+        7 - "x” - a position in the tomogram (an integer value), typically used for subtomogram extraction
+        8 - “y” - a position in the tomogram (an integer value), typically used for subtomogram extraction
+        9 - “z” - a position in the tomogram (an integer value), typically used for subtomogram extraction
+        10 - “shift_x” - shift of the particle in X direction (a float value); to complete position of a particle is given by x + shift_x
+        11 - “shift_y” - shift of the particle in Y direction (a float value); to complete position of a particle is given by y + shift_y
+        12 - “shift_z” - shift of the particle in Z direction (a float value); to complete position of a particle is given by z + shift_z
+        13 - “geom3” - a free geometric property
+        14 - “geom4” - a free geometric property
+        15 - “geom5” - a free geometric property
+        16 - “phi” - a phi angle describing rotation around the first Z axis (following Euler zxz convention)
+        17 - “psi” - a psi angle describing rotation around the second Z axis (following Euler zxz convention)
+        18 - “theta” - a thetha angle describing rotation around the X axis (following Euler zxz convention)
+        19 - “class” - a class of the particle"""
+        coord = Coordinate3D()
+        score = row[0]
+        x, y, z = row[7:10]
+        sx, sy, sz = row[10:13]
+        phi, psi, theta = row[16:19]
+        matrix = self.eulerAngles2matrix(phi, theta, psi, sx, sy, sz)
+        coord.setScore(score)
+        coord.setVolume(tomo)
+        coord.setPosition(x, y, z, BOTTOM_LEFT_CORNER)
+        coord.setMatrix(matrix)
+        return coord
+
+    @staticmethod
+    def eulerAngles2matrix(tdrot, tilt, narot, shiftx, shifty, shiftz):
+        # Relevant info:
+        #   * GapStop's transformation system is ZXZ
+        #   * Sscipion = R * (-Sgapstop) ==> Sgapstop = Rinv * (-Sscipion)
+        M = np.eye(4)
+        sx = float(shiftx)
+        sy = float(shifty)
+        sz = float(shiftz)
+        Sgapstop = np.array([sx, sy, sz])
+        tdrot = np.deg2rad(float(tdrot))
+        narot = np.deg2rad(float(narot))
+        tilt = np.deg2rad(float(tilt))
+        R = transformations.euler_matrix(tdrot, tilt, narot, axes='szxz')
+        R = R[:3, :3]
+        Sscipion = - np.dot(R, Sgapstop)
+        M[:3, :3] = R
+        M[:3, 3] = Sscipion
+        return M
+
     def _getFormAttrib(self, attribName: str, returnPointer: bool = False) -> Union[SetOfTiltSeries,
     SetOfTomograms, SetOfCTFTomoSeries, Volume, Pointer, None]:
         inTsPointer = getattr(self, attribName, None)
@@ -400,7 +497,7 @@ angles_numbering=0)
     def _convertOrLinkVolume(self, inVolume: Volume, outVolume: str) -> None:
         """Converts a volume into a compatible MRC file or links it if already compatible"""
         inFn = inVolume.getFileName()
-        # If compatible with dynamo. Attention!! Assuming is not a stack of mrc volumes!!
+        # If compatible with gapstop. Attention!! Assuming is not a stack of mrc volumes!!
         if getExt(inFn) == MRC:
             createLink(abspath(inFn), outVolume)
         else:
@@ -478,6 +575,24 @@ angles_numbering=0)
             HP_RAD,
             BINNING,
             TILING]
+
+    def _loadParticleList(self, tsId: str) -> str:
+        """The particle list is encoded in a .em file. Thus, it will be loaded and decoded. The data represented
+        on each column is explained in https://cryocat.readthedocs.io/latest/user_guide/motl_basics.html"""
+        particlesEmFile = self._getResultsFile(tsId, PARTICLE_LIST_FILE)
+        outParticlesNpyFile = self._getResultsFile(tsId, PARTICLES_DATA_FILE, NPY)
+        codePatch = f""" 
+import emfile 
+import numpy as np
+
+_, data = emfile.read('{particlesEmFile}')
+np.save('{outParticlesNpyFile}', data)
+        """
+        readEmParticlePythonFile = self._getExtraPath('readEmParticles.py')
+        with open(readEmParticlePythonFile, "w") as pyFile:
+            pyFile.write(codePatch)
+        Plugin.runGapStop(self, PYTHON, readEmParticlePythonFile, isCryoCatExec=True)
+        return outParticlesNpyFile
 
     # --------------------------- INFO functions ------------------------------
     def _validate(self) -> List[str]:
